e402607d924cb81ef43a56a8262a45d81f69332f
Artem T <artem.trush@mpsdevelopment.com>
2023-11-11 00:22:53 +0200

    Add default constructors

--- src/main/java/com/example/kitty/entities/LatLong.java
+++ src/main/java/com/example/kitty/entities/LatLong.java
@@ -2,10 +2,12 @@ package com.example.kitty.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 @AllArgsConstructor
 @Accessors(chain = true)
+@NoArgsConstructor
 @Data
 public class LatLong {
     private Double latitude;
--- src/main/java/com/example/kitty/entities/LatLongPair.java
+++ src/main/java/com/example/kitty/entities/LatLongPair.java
@@ -2,9 +2,11 @@ package com.example.kitty.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Accessors(chain = true)
 @Data
 public class LatLongPair {
