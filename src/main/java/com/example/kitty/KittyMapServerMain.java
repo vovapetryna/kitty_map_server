c42906a53f10f28b5774c7d280c0723c5e349bc0
Artem T <artem.trush@mpsdevelopment.com>
2023-11-10 15:11:06 +0200

    Add routing basics

--- src/main/java/com/example/kitty/KittyMapServerMain.java
+++ src/main/java/com/example/kitty/KittyMapServerMain.java
@@ -1,15 +1,44 @@
 package com.example.kitty;
 
+import com.example.kitty.services.GraphHopperService;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.List;
+
 
 @SpringBootApplication
 @EnableMongoRepositories
+@RequiredArgsConstructor
+@AutoConfiguration
 public class KittyMapServerMain {
+
+	private final GraphHopperService graphHopperService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KittyMapServerMain.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			GeoJsonPoint from = new GeoJsonPoint(30.620930191509597, 50.39402865778816);
+			GeoJsonPoint to = new GeoJsonPoint(30.518825271259313, 50.42281479982218);
+			Pair<List<GeoJsonPoint>, Double> routingResults = graphHopperService.getRouteBetweenPoints(from, to);
+			List<GeoJsonPoint> route = routingResults.getLeft();
+			for (GeoJsonPoint point : route) {
+				System.out.println(point.getY() + "," + point.getX() + ",");
+			}
+//            collectionToFile();
+//            System.out.println("saved");
+		};
+	}
 }
---
+++ src/main/java/com/example/kitty/services/GraphHopperService.java
@@ -0,0 +1,123 @@
+package com.example.kitty.services;
+
+import com.graphhopper.GHRequest;
+import com.graphhopper.GHResponse;
+import com.graphhopper.GraphHopper;
+import com.graphhopper.GraphHopperConfig;
+import com.graphhopper.ResponsePath;
+import com.graphhopper.config.Profile;
+import com.graphhopper.util.PointList;
+import com.graphhopper.util.shapes.GHPoint3D;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class GraphHopperService {
+
+    private GraphHopper hopper;
+
+    private final String routingFolderLocation = "routing";
+    private final String osmFileName = "roads_Kyiv.osm";
+    private final String graphCacheName = "kyivRoadsCache";
+
+    static List<GeoJsonPoint> convertPointListToWaypointList(PointList pointList) {
+        List<GeoJsonPoint> wayPoints = new ArrayList<>();
+        for (GHPoint3D point : pointList) {
+            wayPoints.add(new GeoJsonPoint(point.lon, point.lat));
+        }
+        return wayPoints;
+    }
+
+    public static ResponsePath routing(GraphHopper hopper, GHRequest req) {
+        // simple configuration of the request object
+        GHResponse rsp = hopper.route(req);
+
+        // handle errors
+        if (rsp.hasErrors())
+            throw new RuntimeException(rsp.getErrors().toString());
+
+        // use the best path, see the GHResponse class for more possibilities.
+        return rsp.getBest();
+    }
+
+
+    public Pair<List<GeoJsonPoint>, Double> getRouteBetweenPoints(GeoJsonPoint from, GeoJsonPoint to) {
+        GraphHopper hopper = getGraphHopperInstance();
+        GHRequest req = new GHRequest(from.getY(), from.getX(), to.getY(), to.getX())
+            .setProfile("foot");
+        ResponsePath responsePath = routing(hopper, req);
+        PointList pointList = responsePath.getPoints();
+        Double pathDistance = responsePath.getDistance();
+        return Pair.of(convertPointListToWaypointList(pointList), pathDistance); //getSimplifyPointsList(convertPointListToWaypointList(pointList));
+    }
+
+//    public List<AiWayPointDto> getSimplifyPointsList(List<AiWayPointDto> originalList) {
+//        if (originalList.size() < 2) {
+//            return originalList;
+//        }
+//        AiWayPointDto[] pointArray = new AiWayPointDto[originalList.size()];
+//        int i = 0;
+//        for (AiWayPointDto wayPoint : originalList) {
+//            pointArray[i++] = wayPoint;
+//        }
+//        Simplify<AiWayPointDto> aut = new Simplify<>(new AiWayPointDto[0]);
+//        AiWayPointDto[] pointsActual = aut.simplify(pointArray, 0.005, true);
+//        log.debug("Start length=  {}", pointArray.length);
+//        log.debug("Result length=  {}", pointsActual.length);
+//        return new ArrayList<>(Arrays.asList(pointsActual));
+//    }
+
+
+    private synchronized GraphHopper getGraphHopperInstance() {
+
+        if (hopper == null) {
+
+            String cacheLocation = routingFolderLocation + "/" + graphCacheName;
+            String osmFileLocation = routingFolderLocation + "/" + osmFileName;
+
+            hopper = new GraphHopper().init(
+                new GraphHopperConfig().
+                    putObject("datareader.file", osmFileLocation).
+                    putObject("datareader.dataaccess", "RAM").
+                    putObject("graph.location", cacheLocation).
+                    putObject("graph.vehicles", "foot").
+                    putObject("import.osm.ignored_highways", ""). //ootway,cycleway,path,pedestrian,steps,residential,track
+                    putObject("index.max_region_search", 100).
+                    putObject("ch.disable", true).
+                    setProfiles(Collections.singletonList(
+                        new Profile("foot").setVehicle("foot").setWeighting("fastest").setTurnCosts(false)
+                    ))).setOSMFile(osmFileLocation);
+            hopper.importOrLoad();
+        }
+        return hopper;
+    }
+
+    private static void printPoints(PointList pointList) {
+        System.out.println("Printing points");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < pointList.size(); ++i) {
+            if (i > 0) {
+                sb.append(",\n");
+            }
+            sb.append(pointList.getLat(i));
+            sb.append(',');
+            sb.append(pointList.getLon(i));
+            if (pointList.is3D()) {
+                sb.append(',');
+                sb.append(pointList.getEle(i));
+            }
+        }
+        System.out.println(sb);
+    }
+
+}
