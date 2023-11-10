ae17778103a208590759ea4266feaafe9b6563c8
Volodymyr Petryna <volodymyr.petryna@nordstrom.com>
2023-11-10 16:47:32 +0200

    read from cube creds

--- src/main/java/com/example/kitty/configs/AWSConfig.java
+++ src/main/java/com/example/kitty/configs/AWSConfig.java
@@ -1,6 +1,8 @@
 package com.example.kitty.configs;
 
-import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -11,9 +13,14 @@ import org.springframework.context.annotation.Configuration;
 public class AWSConfig {
     @Bean
     public AmazonS3 amazonS3() {
+        String accessKey = System.getenv("CLOUDCUBE_ACCESS_KEY_ID");
+        String secretKey = System.getenv("CLOUDCUBE_SECRET_ACCESS_KEY");
+
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
         AmazonS3 s3client = AmazonS3ClientBuilder
                 .standard()
-                .withCredentials(new EnvironmentVariableCredentialsProvider())
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(Regions.US_EAST_1)
                 .build();
         return s3client;
