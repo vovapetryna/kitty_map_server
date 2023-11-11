a0454c676b5707185dee536ca9caae03edb7ff0e
Volodymyr Petryna <volodymyr.petryna@nordstrom.com>
2023-11-11 09:39:12 +0200

    brake mapper

--- src/main/java/com/example/kitty/utils/GeoJsonDeserializer.java
+++ src/main/java/com/example/kitty/utils/GeoJsonDeserializer.java
@@ -9,31 +9,14 @@ import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import java.io.IOException;
 
 public class GeoJsonDeserializer extends JsonDeserializer<GeoJsonPoint> {
-    private final static String GEOJSON_TYPE_POINT = "Point";
-
-    private final static String JSON_KEY_GEOJSON_TYPE = "type";
-    private final static String JSON_KEY_GEOJSON_COORDS = "coordinates";
-
-
     @Override
     public GeoJsonPoint deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
 
         final JsonNode tree = jp.getCodec().readTree(jp);
         System.out.println(tree);
-        final String type = tree.get(JSON_KEY_GEOJSON_TYPE).asText();
-        final JsonNode coordsNode = tree.get(JSON_KEY_GEOJSON_COORDS);
+        final double x = tree.get("x").asDouble();
+        final double y = tree.get("y").asDouble();
 
-        double x = 0;
-        double y = 0;
-        if (GEOJSON_TYPE_POINT.equalsIgnoreCase(type)) {
-            x = coordsNode.get(0).asDouble();
-            y = coordsNode.get(1).asDouble();
-        } else {
-            System.out.println(String.format("No logic present to deserialize %s ", tree.asText()));
-        }
-
-        final GeoJsonPoint point = new GeoJsonPoint(x, y);
-
-        return point;
+        return new GeoJsonPoint(x, y);
     }
 }
