d43e80213a8f3cbbf912523528dcf367920f3760
Volodymyr Petryna <volodymyr.petryna@nordstrom.com>
2023-11-11 07:20:43 +0200

    add mapping module

--- src/main/java/com/example/kitty/configs/AppConfig.java
+++ src/main/java/com/example/kitty/configs/AppConfig.java
@@ -1,13 +1,22 @@
 package com.example.kitty.configs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.core.geo.GeoJsonModule;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Primary
 @Configuration
@@ -17,9 +26,17 @@ public class AppConfig extends WebMvcConfigurationSupport {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-            .info(new Info().title("UavSim Instructor service")
-                .description("UavSim Instructor service")
-                .version("Version 0.1"));
+                .info(new Info().title("UavSim Instructor service")
+                        .description("UavSim Instructor service")
+                        .version("Version 0.1"));
+    }
+    
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json()
+                .modules(new GeoJsonModule())
+                .build();
+        converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
     }
 
 }
